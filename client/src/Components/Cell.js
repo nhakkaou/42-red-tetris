bac7b9e6bfb80ab6290fdae0bd57990380d56932
nhakkaou <noureddine.hak@gmail.com>
2021-03-05 23:57:39 +0000

    Level And Score

--- client/src/Components/Cell.js
+++ client/src/Components/Cell.js
@@ -6,4 +6,4 @@ const Cell = ({type}) => (
     <StyledCell type={type} color={Tetrominos[type].color}></StyledCell>
 )
 
-export default Cell;
+export default React.memo(Cell);
--- client/src/Components/Tetris.js
+++ client/src/Components/Tetris.js
@@ -6,15 +6,16 @@ import Help from "./Help"
 import { checkcollision, Createstage } from '../gameHelper'
 import { StyledtetrisWrapper, StyledTetris } from "./styling/StyledTetris"
 import { useStage, usePlayer, useInterval } from '../hooks'
-
+import { useGameStatus } from '../hooks/useGameStatus'
 
 const Tetris = () => {
     const [dropTime, setDropTime] = useState(null);
     const [gameOver, setGameOver] = useState(false);
 
     const [player, updatePlayerPos, resetPlayer, playerRotate] = usePlayer();
-    const [stage, setStage] = useStage(player, resetPlayer);
+    const [stage, setStage, rowsCleared] = useStage(player, resetPlayer);
 
+    const [score, setScore, rows, setRows, level, setLevel] = useGameStatus(rowsCleared)
     //console.log('re-render');
 
     const movePlayer = dir => {
@@ -27,9 +28,17 @@ const Tetris = () => {
         setDropTime(1000);
         resetPlayer();
         setGameOver(false)
+        setScore(0);
+        setRows(0)
+        setLevel(0);
     }
 
     const drop = () => {
+        if (rows > level + 1 * 10)
+        {
+            setLevel(prev => prev + 1)
+            setDropTime(1000 / level + 1 + 200)
+        }
         if (!checkcollision(player, stage, { x: 0, y: 1 }))
             updatePlayerPos({ x: 0, y: 1, collided: false })
         else {
@@ -43,7 +52,7 @@ const Tetris = () => {
 
     const keyUp = ({ keyCode }) => {
         if (!gameOver && keyCode === 40) {
-            setDropTime(1000);
+            setDropTime(1000 / level + 1 + 200);
         }
     }
 
@@ -74,10 +83,10 @@ const Tetris = () => {
             <StyledTetris>
                 <Stage stage={stage} />
                 <aside>
+                    <Display text={`Score: ${score}`} />
                     {gameOver ? <Display gameOver={gameOver} text="Game Over" /> : (
-                        <div>
-                            <Display text="Score" />
-                            <Display text="Level" />
+                        <div>  
+                            <Display text={`Level: ${level}`} />
                             <Help />
 
                         </div>
