ee15ba11ece47c80194fcc8fddd89b47c1e5fe33
Noreddine Hakkaoui <nhakkaou@e2r3p8.1337.ma>
2021-03-06 18:59:07 +0100

    Help

--- client/src/Components/help.js
+++ client/src/Components/help.js
@@ -2,14 +2,32 @@ import React from "react"
 import {Stylehelp} from "./styling/StyledHelp"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowUp, faArrowDown, faArrowRight, faArrowLeft } from '@fortawesome/free-solid-svg-icons'
+import styled from "styled-components"
 
+
+const Li = styled.li`
+
+`
+const Ul = styled.ul`
+    list-style: none;
+    display: inline-block;
+    width: 100%;
+    height: 100%;
+    margin: 0;
+    font-size: 20px;
+    padding-top: 40%;
+    font-family: 'Hanalei', cursive;
+`
 const Help = () => (
-    <Stylehelp>HELP
+    <Stylehelp>
         <div>
-        <FontAwesomeIcon icon={faArrowUp} />
-        <FontAwesomeIcon icon={faArrowDown} />
-        <FontAwesomeIcon icon={faArrowRight} />
-        <FontAwesomeIcon icon={faArrowLeft} />
+        HELP:
+        <Ul>
+        <Li>Rotation <FontAwesomeIcon icon={faArrowUp} /></Li>
+        <Li>Drop <FontAwesomeIcon icon={faArrowDown} /></Li>
+        <Li>Right <FontAwesomeIcon icon={faArrowRight} /></Li>
+        <Li>Left <FontAwesomeIcon icon={faArrowLeft} /></Li>
+        </Ul>
         </div> 
     </Stylehelp>
 )
