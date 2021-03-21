9b7c21b370a7fe744252ca24512c0a8292eacc97
Ilyas Sifeddine <sifeddineilyass@gmail.com>
2021-03-21 15:08:47 +0100

    enhance random tetromino function

--- client/src/tetrominos.js
+++ client/src/tetrominos.js
@@ -62,9 +62,14 @@ export const Tetrominos = {
   },
 };
 
+const randomIntFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+var pieces = [];
 export const randomTetromino = () => {
-  const tetrominoStr = "IJLDSTZ";
-  const randomTetro =
-    tetrominoStr[Math.floor(Math.random() * tetrominoStr.length)];
+  if (pieces.length == 0)
+    pieces = ["I", "I", "I", "I", "J", "J", "J", "J", "L", "L", "L", "L", "D", "D", "D", "D", "S", "S", "S", "S", "T", "T", "T", "T", "Z", "Z", "Z", "Z"];
+  const randomTetro = pieces.splice(randomIntFromInterval(0, pieces.length - 1), 1)[0];
   return Tetrominos[randomTetro];
 };
