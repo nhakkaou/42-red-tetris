d2bf05c865e09599066bc3261ba0c70f30f870d3
Ilyas Sifeddine <sifeddineilyass@gmail.com>
2021-04-22 17:21:43 +0000

    merged + errors to fix

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -12,7 +12,9 @@ function Home() {
   });
   const dispatch = useDispatch();
   if (window.location.hash.match(/[a-zA-Z0-9]+[[a-zA-Z0-9]+]/)) {
+
     let a = window.location.hash.match(/(?<room>[a-zA-Z0-9]+)\[(?<usr>[a-zA-Z0-9]+)\]/);
+    console.log('dekhel hna', a)
     socket.emit("join", { user: a.groups.usr, room: a.groups.room });
     dispatch({ type: UPDATE_NAME, data: a.groups.room });
     return (
