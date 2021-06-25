d1484e075859f03a18f4cda2c1b4530ec673a893
Ilyas SIFEDDINE <sifeddineilyass@gmail.com>
2021-06-25 17:56:40 +0100

    API Classification

---
+++ API/src/Game.js
@@ -0,0 +1,119 @@
+class Game {
+  leaveRoom = (socket, Players, io, Rooms) => {
+    return new Promise((resolve, reject) => {
+      let cmptr = 0;
+      let winner = {};
+      let tmp = Players.find((el) => el.socketId === socket.id);
+      if (tmp) {
+        socket.leave(tmp.room);
+        let tmp2 = Players.filter((el) => el.socketId !== socket.id);
+        Players = tmp2;
+        for (let i = 0; i < Players.length; i++) {
+          if (Players[i].room === tmp.room && !Players[i].hasLost) {
+            winner = Players[i];
+            cmptr++;
+          }
+        }
+        if (cmptr === 1) io.sockets.in(tmp.room).emit("Winner", winner);
+        if (Players.filter((el) => el.room === tmp.room).length === 0)
+          Rooms = Rooms.filter((el) => el.name !== tmp.room);
+        if (tmp.admin === true) {
+          let i = Players.findIndex((el) => el.room === tmp.room);
+          Players[i] = {
+            ...Players[i],
+            admin: true,
+          };
+          socket.to(Players[i].socketId).emit("Update Admin", {
+            user: Players[i],
+            is_admin: Players[i].admin,
+          });
+        }
+        io.sockets.in(tmp.room).emit(
+          "new member",
+          Players.filter((e) => e.room === tmp.room)
+        );
+        resolve({
+          Players: Players,
+          Rooms: Rooms,
+        });
+      }
+    });
+  };
+  joinRoom = (socket, data, io, Rooms, Players) => {
+    return new Promise((resolve, reject) => {
+      if (io.sockets.adapter.rooms.get(data.room)) {
+        const clients = io.sockets.adapter.rooms.get(data.room);
+        const tmpRoom = Rooms.find((el) => el.name === data.room);
+        const numClients = clients ? clients.size : 0;
+        if (tmpRoom && tmpRoom.startGame === true)
+          return socket.emit("TOASTIFY", {
+            type: "error",
+            message: "The game has already started",
+          });
+        if (numClients + 1 > 5) {
+          const message = { type: "error", message: "Room is full!" };
+          socket.emit("TOASTIFY", message);
+          return;
+        } else {
+          let a = Players.find(
+            (element) =>
+              element.user === data.user && element.room === data.room
+          );
+
+          if (!a || a.user !== data.user) {
+            if (tmpRoom && tmpRoom.mode === "Solo") {
+              socket.emit("TOASTIFY", {
+                type: "error",
+                message: "Room solo!",
+              });
+              return "";
+            }
+            Players.push({
+              admin: false,
+              socketId: socket.id,
+              user: data.user,
+              hasLost: false,
+              room: data.room,
+              score: 0,
+            });
+            socket.join(data.room);
+            const message = { type: "success", message: "Joined room!" };
+            socket.emit("Join_success", data);
+            socket.emit("TOASTIFY", message);
+          } else
+            return socket.emit("TOASTIFY", {
+              type: "error",
+              message: "Username Already existe",
+            });
+        }
+      } else {
+        socket.join(data.room);
+        let a = Players.find(
+          (element) => element.user === data.user && element.room === data.room
+        );
+        if (!a || a.user !== data.user) {
+          Players.push({
+            admin: true,
+            socketId: socket.id,
+            user: data.user,
+            hasLost: false,
+            room: data.room,
+            score: 0,
+          });
+        }
+        Rooms.push({ name: data.room, mode: data.mode, startGame: false });
+        const message = { type: "success", message: "Created room!" };
+        socket.emit("Join_success", { ...data, is_admin: true });
+        socket.emit("TOASTIFY", message);
+      }
+      let arr = Players.filter((element) => element.room === data.room);
+      io.sockets.in(data.room).emit("new member", arr);
+      resolve({
+        Players: Players,
+        Rooms: Rooms,
+      });
+    });
+  };
+}
+
+module.exports = Game;
--- API/src/helpers.js
+++ API/src/helpers.js
@@ -1,19 +1,19 @@
-let arr = [];
-const randomIntFromInterval = (min, max) => {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-};
-const randomTetromino = () => {
-  // prettier-ignore
-  let pieces = ["I", "I", "I", "I", "J", "J", "J", "J", "L", "L", "L", "L", "D", "D", "D", "D", "S", "S", "S", "S", "T", "T", "T", "T", "Z", "Z", "Z", "Z"];
-  while (arr.length <= 10) {
-    const randomTetro = pieces.splice(
-      randomIntFromInterval(0, pieces.length - 1),
-      1
-    )[0];
-    arr.push(randomTetro);
-  }
-  return arr;
-};
+// let arr = [];
+// const randomIntFromInterval = (min, max) => {
+//   return Math.floor(Math.random() * (max - min + 1) + min);
+// };
+// const randomTetromino = () => {
+//   // prettier-ignore
+//   let pieces = ["I", "I", "I", "I", "J", "J", "J", "J", "L", "L", "L", "L", "D", "D", "D", "D", "S", "S", "S", "S", "T", "T", "T", "T", "Z", "Z", "Z", "Z"];
+//   while (arr.length <= 10) {
+//     const randomTetro = pieces.splice(
+//       randomIntFromInterval(0, pieces.length - 1),
+//       1
+//     )[0];
+//     arr.push(randomTetro);
+//   }
+//   return arr;
+// };
 
 const validateName = (name) => {
   if (name.trim() === "") return false;
@@ -21,4 +21,4 @@ const validateName = (name) => {
   return false;
 };
 
-module.exports = { randomTetromino, validateName };
+module.exports = { validateName };
